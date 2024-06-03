ca2055b4a0c04ab0cbb14cb5ade9bede9d837891
HelpfulFish <learning_open_source_dev@proton.me>
2024-06-03 20:38:32 +1200

    feat: swagger jsdocs for endpoints

--- src/api/health/index.js
+++ src/api/health/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET health */
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     tags:
+ *       - Health Check
+ *     summary: Checks if the server is running
+ *     responses:
+ *       200:
+ *         description: Server is running
+ */
 router.get('/', function(req, res, next) {
   res.send('OK');
 });
--- src/api/mal/oauth.js
+++ src/api/mal/oauth.js
@@ -3,17 +3,121 @@ var router = express.Router();
 var qs = require('qs');
 const axios = require('axios');
 
+/**
+ * @swagger
+ * /mal/oauth/authorize:
+ *   post:
+ *     tags:
+ *      - MyAnimeList OAuth2
+ *     summary: Redirects to the MAL OAuth2 authorization page
+ *     parameters:
+ *       - in: header
+ *         name: mal_client_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: header
+ *         name: mal_challenge
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       302:
+ *         description: Redirect to the MAL OAuth2 authorization page
+ */
 router.post('/authorize', function(req, res) {
   const clientId  = req.headers.mal_client_id;
   const challenge = req.headers.mal_challenge;
   res.redirect(`https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${clientId}&code_challenge=${challenge}&code_challenge_method=plain`);
 });
 
+/**
+ * @swagger
+ * tags:
+ *    - mal
+ * /mal/oauth/callback:
+ *   get:
+ *     tags:
+ *      - MyAnimeList OAuth2
+ *     summary: Callback endpoint for MAL OAuth2
+ *     parameters:
+ *       - in: query
+ *         name: code
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the authorization code
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: string
+ */
 router.get('/callback', function(req, res) {
   var code = req.query.code;
   res.json({ code: code });
 });
 
+/**
+ * @swagger
+ * tags:
+ *    - mal
+ * /mal/oauth/token:
+ *   get:
+ *     tags:
+ *      - MyAnimeList OAuth2
+ *     summary: Exchanges the authorization code for an access and refresh token
+ *     parameters:
+ *       - in: header
+ *         name: mal_code
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: header
+ *         name: mal_client_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: header
+ *         name: mal_client_secret
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: header
+ *         name: mal_code_verifier
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the access token and related information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 access_token:
+ *                   type: string
+ *                 refresh_token:
+ *                   type: string
+ *                 token_type:
+ *                   type: string
+ *                 expires_in:
+ *                   type: integer
+ *       400:
+ *         description: Error exchanging code for token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
 router.get('/token', async function(req, res) {
   const code          = req.headers.mal_code;
   const clientId      = req.headers.mal_client_id;
